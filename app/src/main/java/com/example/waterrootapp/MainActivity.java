a3d1952b2761e4775dec95647d8bf62f302f0451
DanielA <danironi523@gmail.com>
2019-03-25 12:14:19 -0400

    This is a test to see if I can update the files

--- app/src/main/java/com/example/waterrootapp/MainActivity.java
+++ app/src/main/java/com/example/waterrootapp/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,9 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     }
     public void onWater(View v){
-        TextView output= (TextView) findViewById(R.id.output);
+        TextView output= findViewById(R.id.nextWater);
         output.setText("Pressed");
+        //Example code to write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("test1");
+
+        myRef.setValue("Hello, World!");
+        //end Example code
     }
 }
