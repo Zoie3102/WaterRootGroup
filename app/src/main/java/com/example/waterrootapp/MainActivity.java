cb0952d0d871360fa6194c3fb2870a0045cc9598
DanielA <danironi523@gmail.com>
2019-03-26 12:17:13 -0400

    Renaming images

--- app/src/main/java/com/example/waterrootapp/MainActivity.java
+++ app/src/main/java/com/example/waterrootapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.waterrootapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseApp.initializeApp(this);
+//        FirebaseApp.initializeApp(this);
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("test1");
 
 
     }
@@ -23,11 +26,13 @@ public class MainActivity extends AppCompatActivity {
         TextView output= (TextView) findViewById(R.id.nextWater);
         output.setText("Pressed");
         //Example code to write a message to the database
-        FirebaseApp.initializeApp(this);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("test1");
-
-        myRef.setValue("Hello, World!");
+//        FirebaseApp.initializeApp(this);
+//        if (FirebaseApp==null)
+//        Log.d("got here");
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("test1");
+//
+//        myRef.setValue("Hello, World!");
         //end Example code
     }
 }
