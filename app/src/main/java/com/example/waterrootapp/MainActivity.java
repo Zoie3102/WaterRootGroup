c0c60a93c3cd6e1b3d02ae0237dee7cb29cb2149
nilaymclaren <npmclaren@wpi.edu>
2019-03-29 11:54:25 -0400

    Created new xml for popup and started coding the popup

--- app/src/main/java/com/example/waterrootapp/MainActivity.java
+++ app/src/main/java/com/example/waterrootapp/MainActivity.java
@@ -19,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_main);
 //        FirebaseApp.initializeApp(this);
 //        FirebaseDatabase database = FirebaseDatabase.getInstance();
