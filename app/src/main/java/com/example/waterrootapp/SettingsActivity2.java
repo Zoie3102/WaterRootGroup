0d03cbf16eefd6d9abdf45dea77830817eb23124
nilaymclaren <npmclaren@wpi.edu>
2019-04-24 11:00:04 -0400

    made the state of the time of water stay after closing app

--- app/src/main/java/com/example/waterrootapp/SettingsActivity2.java
+++ app/src/main/java/com/example/waterrootapp/SettingsActivity2.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -14,10 +15,12 @@ import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import java.sql.Time;
 import java.util.Timer;
 
+import static android.provider.Contacts.SettingsColumns.KEY;
 import static android.support.constraint.Constraints.TAG;
 import static com.example.waterrootapp.SplashScreenActivity.firstTime;
 import static com.example.waterrootapp.SplashScreenActivity.sharedPreferences;
@@ -28,8 +31,7 @@ public class SettingsActivity2 extends AppCompatActivity {
     public static final String PREFS_NAME = "MyPrefsFile";
    public  SharedPreferences myPrefs;
     public  SharedPreferences.Editor editor2;
-
-
+    public static final String LAST_TEXT = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -146,14 +148,28 @@ else {
         final EditText timeOfWater =  findViewById(R.id.editText);
 
 
+        SharedPreferences settings4 = getSharedPreferences(PREFS_NAME, 0);
+        String silent4 = settings.getString("timeofwaterkey", null);
+        timeOfWater.setText(silent4);
 
         timeOfWater.addTextChangedListener(new TextChangedListener<EditText>(timeOfWater) {
             @Override
             public void onTextChanged(EditText target, Editable s) {
                 String time = timeOfWater.getText().toString();
+                Log.d(TAG, time);
                 Intent timeIntent = new Intent(SettingsActivity2.this, TimerService.class);
+
                 timeIntent.putExtra("timeofday", time);
+
+                SharedPreferences settings4 = getSharedPreferences(PREFS_NAME, 0);
+                SharedPreferences.Editor editor4 = settings4.edit();
+                editor4.putString("timeofwaterkey", time);
+                editor4.commit();
+
             }
+
+
+
         });
     }
 
@@ -180,11 +196,15 @@ else {
 
         @Override
         public void afterTextChanged(Editable s) {
+
             this.onTextChanged(target, s);
+
         }
 
         public abstract void onTextChanged(T target, Editable s);
     }
+
+
 
 
 
--- app/src/main/java/com/example/waterrootapp/TimerService.java
+++ app/src/main/java/com/example/waterrootapp/TimerService.java
@@ -320,6 +320,9 @@ Calendar calendar;
         }
     }
 
+
+
+
     
 
 
