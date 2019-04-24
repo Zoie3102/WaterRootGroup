906f71e93ff772cfcd504fdbd0382907b3903dc4
DanielA <danironi523@gmail.com>
2019-04-24 11:26:55 -0400

    Water Log

--- app/src/main/java/com/example/waterrootapp/WaterLogActivity.java
+++ app/src/main/java/com/example/waterrootapp/WaterLogActivity.java
@@ -6,14 +6,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 public class WaterLogActivity extends AppCompatActivity {
 
+    private static String waterLogText="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +41,25 @@ public class WaterLogActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference waterLog = database.getReference("waterLog");
         output.setText(waterLog.toString());
+
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+//                text=dataSnapshot.getValue().toString();
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w("firebase", "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+        waterLog.addValueEventListener(postListener);
+//        Query test=waterLog;
+//        output.setText(test.toString());
 
     }
 }
